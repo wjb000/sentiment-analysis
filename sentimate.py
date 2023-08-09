3ceb5e84d73ecd4654e7e9c086bf9a2db503f1f4
net1ife <127271713+net1ife@users.noreply.github.com>
2023-08-09 16:41:14 -0500

    Update sentimate.py

--- sentimate.py
+++ sentimate.py
@@ -1,24 +1,41 @@
 import requests
 import nltk
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-from nltk.classify import NaiveBayesClassifier
 from nltk.tokenize import word_tokenize
+from nltk.classify import NaiveBayesClassifier
 
 # Constants
 NEWSAPI_KEY = "YOUR_NEWSAPI_KEY"
 NEWSAPI_ENDPOINT = "https://newsapi.org/v2/everything"
 
-# --- Setup for sentiment analysis ---
+def fetch_articles(ticker):
+    query = ticker + " stock"
+    params = {
+        'q': query,
+        'apiKey': NEWSAPI_KEY,
+        'language': 'en',
+        'sortBy': 'relevancy',
+        'pageSize': 100
+    }
+    response = requests.get(NEWSAPI_ENDPOINT, params=params)
+    if response.status_code != 200:
+        print("Error fetching news:", response.text)
+        return []
 
-# [For demonstration, I'll use the mock dataset. Ideally, use a proper dataset]
-documents = [([...], "positive"), ([...], "negative")]  # Example data format
+    articles = response.json().get('articles', [])
+    return [article['description'] or article['title'] for article in articles if article['description'] or article['title']]
 
-# Preprocessing setup
-nltk.download('stopwords')
-nltk.download('punkt')
-stop_words = set(stopwords.words("english"))
-lemmatizer = WordNetLemmatizer()
+# Fetch articles for a ticker
+ticker = input("Enter the stock ticker for which you want to fetch news articles: ")
+articles = fetch_articles(ticker)
+
+# For demonstration, manually label the first N articles
+N = 10  # or however many you're comfortable labeling
+print("\nManually label the first few articles:")
+documents = []
+for i, article in enumerate(articles[:N]):
+    print(f"\nArticle {i+1}:\n{article}")
+    label = input("Enter sentiment (positive/negative/neutral): ")
+    documents.append((word_tokenize(article), label))
 
 # Define a feature extractor
 all_words = nltk.FreqDist(w.lower() for words, sentiment in documents for w in words)
@@ -31,61 +48,12 @@ def document_features(document):
         features['contains({})'.format(word)] = (word in document_words)
     return features
 
-# Train a classifier
+# Train a classifier using the manually labeled articles
 featuresets = [(document_features(d), c) for (d, c) in documents]
 classifier = NaiveBayesClassifier.train(featuresets)
 
-# --- News fetching and analysis ---
-
-def fetch_articles(ticker):
-    query = ticker + " stock"  # Modify this query if needed
-    params = {
-        'q': query,
-        'apiKey': NEWSAPI_KEY,
-        'language': 'en',
-        'sortBy': 'relevancy',
-        'pageSize': 100  # Fetch 100 articles
-    }
-    response = requests.get(NEWSAPI_ENDPOINT, params=params)
-    if response.status_code != 200:
-        print("Error fetching news:", response.text)
-        return []
-
-    articles = response.json().get('articles', [])
-    return [article['description'] or article['title'] for article in articles if article['description'] or article['title']]
-
-def analyze_articles(articles):
-    results = []
-    for article in articles:
-        words = word_tokenize(article)
-        words = [lemmatizer.lemmatize(word) for word in words if word not in stop_words]
-        features = document_features(words)
-        sentiment = classifier.classify(features)
-        results.append((article, sentiment))
-    return results
-
-def main():
-    ticker = input("Enter the stock ticker for sentiment analysis: ")
-    articles = fetch_articles(ticker)
-    sentiments = analyze_articles(articles)
-
-    positive_count, negative_count, neutral_count = 0, 0, 0
-
-    # Display and count results
-    for article, sentiment in sentiments:
-        if sentiment == "positive":
-            positive_count += 1
-        elif sentiment == "negative":
-            negative_count += 1
-        else:
-            neutral_count += 1
-        print(f"Article: {article}\nSentiment: {sentiment}\n{'-' * 50}")
-
-    # Summary
-    print(f"\nSummary for {ticker}:")
-    print(f"Positive articles: {positive_count}")
-    print(f"Negative articles: {negative_count}")
-    print(f"Neutral articles: {neutral_count}")
-
-if __name__ == '__main__':
-    main()
+# Analyze the remaining articles
+for article in articles[N:]:
+    words = word_tokenize(article)
+    sentiment = classifier.classify(document_features(words))
+    print(f"\nArticle: {article}\nPredicted Sentiment: {sentiment}\n{'-' * 50}")
