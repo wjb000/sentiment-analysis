478e8a56141c3fb582fbbe2d92da090ce298fd65
net1ife <127271713+net1ife@users.noreply.github.com>
2023-08-10 17:41:34 -0500

    Update sentimate.py

--- sentimate.py
+++ sentimate.py
@@ -1,5 +1,6 @@
 import requests
 import nltk
+from textblob import TextBlob
 from nltk.tokenize import word_tokenize
 from nltk.classify import NaiveBayesClassifier
 
@@ -24,17 +25,26 @@ def fetch_articles(ticker):
     articles = response.json().get('articles', [])
     return [article['description'] or article['title'] for article in articles if article['description'] or article['title']]
 
+def get_sentiment(text):
+    analysis = TextBlob(text)
+    # Define sentiment based on polarity
+    if analysis.sentiment.polarity > 0.1:
+        return 'positive'
+    elif analysis.sentiment.polarity < -0.1:
+        return 'negative'
+    else:
+        return 'neutral'
+
 # Fetch articles for a ticker
 ticker = input("Enter the stock ticker for which you want to fetch news articles: ")
 articles = fetch_articles(ticker)
 
-# For demonstration, manually label the first N articles
+# Auto-label the first N articles using TextBlob
 N = 10  # or however many you're comfortable labeling
-print("\nManually label the first few articles:")
+print("\nAuto-labeling the first few articles using TextBlob:")
 documents = []
 for i, article in enumerate(articles[:N]):
-    print(f"\nArticle {i+1}:\n{article}")
-    label = input("Enter sentiment (positive/negative/neutral): ")
+    label = get_sentiment(article)
     documents.append((word_tokenize(article), label))
 
 # Define a feature extractor
@@ -48,7 +58,7 @@ def document_features(document):
         features['contains({})'.format(word)] = (word in document_words)
     return features
 
-# Train a classifier using the manually labeled articles
+# Train a classifier using the auto-labeled articles
 featuresets = [(document_features(d), c) for (d, c) in documents]
 classifier = NaiveBayesClassifier.train(featuresets)
 
